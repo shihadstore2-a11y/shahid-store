3da4ea25da205aeab080d18cd4f1160cb0457ce8
Digitaneo <digitaneo@gmail.com>
2026-08-23 04:19:17 +0100

    Add missing cn import in ProductRow

--- src/components/admin/products/ProductRow.tsx
+++ src/components/admin/products/ProductRow.tsx
@@ -11,6 +11,7 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { InlineEditField } from "@/components/admin/InlineEditField";
+import { cn } from "@/lib/utils";
 import {
   calcDiscountPercent,
   type AdminProductRow,
